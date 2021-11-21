441cb169e904412ea63ace7d9e700c4e3237c37f
Matthew Davis <matt@mhwdvs.com>
2021-11-22 01:15:04 +0800

    Add storing of git credentials

--- home/hm.nix
+++ home/hm.nix
@@ -32,6 +32,11 @@ in
       enable = true;
       userName = "mhwdvs";
       userEmail = "matt@mhwdvs.com";
+      extraConfig = {
+      credential.helper = "${
+          pkgs.git.override { withLibsecret = true; }
+        }/bin/git-credential-libsecret";
+    };
     };
   };
 
