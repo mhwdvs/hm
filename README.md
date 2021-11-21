061fdd8a04bb1f78f3247b5d50bcce84ca638cf2
Matthew Davis <matt@mhwdvs.com>
2021-11-22 00:48:14 +0800

    Add hardware nix config

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # hm
-My home-manager configuration
+My nix/home-manager configuration
+
+## What makes this special
+
+- NixOS rebuild script that is accessible system-wide - install and somewhat forget
+
+## Usage
+
+### NixOS
+
+#### Install
+
+- Install NixOS
+- Build machine config
+    - `sudo nixos-rebuild -I nixos-config="machines/<desired machine config directory>/configuration.nix" switch`
+- Set password of user `matthew`
+
+#### Rebuild
+
+- `my-nixos-rebuild`
+
+
+
---
+++ home/hm.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+let
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-21.05.tar.gz";
+in
+{
+  imports = [
+    (import "${home-manager}/nixos")
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  home-manager.users.matthew = {
+    /* Here goes your home-manager config, eg home.packages = [ pkgs.foo ]; */
+    home.packages = 
+      [ pkgs.vim
+	      pkgs.vscodium
+	      pkgs.git
+	      pkgs.clang_12
+        pkgs.cmake
+	      pkgs.ninja
+	      pkgs.fish
+        pkgs.ripgrep-all
+	      pkgs.neofetch
+	      pkgs.starship
+	      pkgs.firefox
+	      pkgs.syncthing
+	      pkgs.libreoffice
+        pkgs.okular
+	      pkgs.gnome.gnome-tweaks ];
+
+    programs.git = {
+      enable = true;
+      userName = "mhwdvs";
+      userEmail = "matt@mhwdvs.com";
+    };
+  };
+
+  # use fish shell
+  users.users.matthew.shell = pkgs.fish;
+}
+
